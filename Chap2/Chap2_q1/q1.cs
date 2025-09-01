b1f2e97a96455de21518fc4d2b512b7cde09f36d
Choi Yusun <43121372+ggstone0523@users.noreply.github.com>
2025-09-01 10:59:32 +0900

    Chap2: finish chap2 study

---
+++ Chap2/Chap2_q1/q1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace Chap2_q1
+{
+    class q1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("여러분, 안녕하세요?\n반갑습니다!\n");
+        }
+    }
+}
---
+++ Chap2/Chap_q2/q2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chap2_q2
+{
+    class q2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
