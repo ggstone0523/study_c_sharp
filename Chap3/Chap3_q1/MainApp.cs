25b38ef4c4372e64dece18a575a53c2c6dcfbd6e
Choi Yusun <43121372+ggstone0523@users.noreply.github.com>
2025-09-03 07:09:47 +0900

    Chap3: Add new q1~q5 answer

---
+++ Chap3/Chap3_q1/MainApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Chap3_q1
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("사각형의 너비를 입력하세요.");
+            string width = Console.ReadLine();
+
+            Console.WriteLine("사각형의 높이를 입력하세요.");
+            string height = Console.ReadLine();
+
+            // 이곳에 사각형의 넓이를 계산하고
+            // 출력하는 루틴을 추가하세요
+            Console.WriteLine($"사각형의 넓이는 {Convert.ToInt16(width) * Convert.ToInt16(height)} 입니다.");
+        }
+    }
+}
---
+++ Chap3/Chap3_q2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Chap3_q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("int a = 7.3은 int 형식의 변수에 부동소수점 형식의 값을 넣으려고 하여 소수점 이하 값에 대한 손실이 발생함.");
+            Console.WriteLine("char d = \"abc\"는 문자 형식의 변수에 문자열을 넣으려고 하여 에러가 발생함.");
+            Console.WriteLine("stirng e = '한'은 문자열 형식의 변수에 문자를 넣으려고 하여 에러가 발생함.");
+        }
+    }
+}
