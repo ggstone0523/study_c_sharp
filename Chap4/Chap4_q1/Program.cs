069a0b19d6d44c449ea343b7e58712d0f403d99a
Choi Yusun <43121372+ggstone0523@users.noreply.github.com>
2025-09-03 07:50:03 +0900

    Chap5: Add q1~q5 answer

---
+++ Chap4/Chap4_q1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Chap4_q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("i++: 식의 계산이 완료된 후 값이 증가함.");
+            Console.WriteLine("++i: 값이 증가된 후 식의 계산이 이루어짐.");
+        }
+    }
+}
