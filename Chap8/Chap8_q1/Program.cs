8ee1b95ff793e3ab48d776ef55b53305c15e56c4
Choi Yusun <43121372+ggstone0523@users.noreply.github.com>
2025-09-22 21:06:36 +0900

    Chap8: Add q1 answer

---
+++ Chap8/Chap8_q1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Numerics;
+
+namespace Chap8_q1
+{
+    interface IRunnable
+    {
+        void Run();
+    }
+    interface IFlyable
+    {
+        void Fly();
+    }
+    class Plane : IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Fly");
+        }
+        public void Ride()
+        {
+            Fly();
+        }
+    }
+    class Car : IRunnable
+    {
+        public void Run()
+        {
+            Console.WriteLine("Run");
+        }
+        public void Ride()
+        {
+            Run();
+        }
+    }
+    class FlyingCar : IRunnable, IFlyable
+    {
+        public void Run()
+        {
+            Console.WriteLine("Run! Run!");
+        }
+        public void Fly()
+        {
+            Console.WriteLine("Fly! Fly!");
+        }
+    }
+    class Answer1 : FlyingCar
+    {
+    }
+    class Answer2
+    {
+        Car car = new Car();
+        Plane plane  = new Plane();
+
+        public void Run()
+        {
+            car.Ride();
+        }
+        public void Fly()
+        {
+            plane.Ride();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Answer1 answer1 = new Answer1();
+            answer1.Run();
+            answer1.Fly();
+
+            Answer2 answer2 = new Answer2();
+            answer2.Run();
+            answer2.Fly();
+        }
+    }
+}
