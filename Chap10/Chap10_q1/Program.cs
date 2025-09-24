1aef18af28af44a05afe99423f96f3bbb9557d92
Choi Yusun <43121372+ggstone0523@users.noreply.github.com>
2025-09-24 22:50:41 +0900

    Chap10: Add q1 answer

---
+++ Chap10/Chap10_q1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Chap10_q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] twoDArray = new int[2, 3] { { 0, 1, 2 }, { 3, 4, 5 } };
+            foreach (int num in twoDArray)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
