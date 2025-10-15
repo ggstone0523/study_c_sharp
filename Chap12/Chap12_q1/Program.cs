b991bc9ce996622351fe598fecb6aa544957b08c
Choi Yusun <43121372+ggstone0523@users.noreply.github.com>
2025-10-15 17:49:08 +0900

    Chap12: Add q1 answer

---
+++ Chap12/Chap12_q1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Chap12_q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+
+            for (int i = 0; i < 10; ++i)
+                arr[i] = i;
+
+            try
+            {
+                for (int i = 0; i < 11; ++i)
+                    Console.WriteLine(arr[i]);
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Range Exception Occur!");
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
