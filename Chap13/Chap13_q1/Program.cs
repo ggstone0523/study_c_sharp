f359efda56305bdfe0318a8ded5ec5bae9f861e1
Choi Yusun <43121372+ggstone0523@users.noreply.github.com>
2025-10-16 09:06:13 +0900

    Chap13: Add q1~2 answer

---
+++ Chap13/Chap13_q1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Chap13_q1
+{
+    delegate int MyDelegate(int a, int b);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate Callback;
+            Callback = delegate (int a, int b)
+            {
+                return a + b;
+            };
+            Console.WriteLine(Callback(3, 4));
+            Callback = delegate (int a, int b)
+            {
+                return a - b;
+            };
+            Console.WriteLine(Callback(7, 5));
+        }
+    }
+}
