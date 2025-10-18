21ff0ddfac3ace013dcee7d61f8214d2b23ddfde
Choi Yusun <43121372+ggstone0523@users.noreply.github.com>
2025-10-18 14:57:50 +0900

    Chap15: Add q1~2 answer

---
+++ Chap15/Chap15_q1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Chap15_q1
+{
+    class Car
+    {
+        public int Cost { get; set; }
+        public int MaxSpeed { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] cars =
+            {
+                new Car(){Cost= 56, MaxSpeed= 120},
+                new Car(){Cost= 70, MaxSpeed= 150},
+                new Car(){Cost= 45, MaxSpeed= 180},
+                new Car(){Cost= 32, MaxSpeed= 200},
+                new Car(){Cost= 80, MaxSpeed= 280}
+            };
+            
+            var selected = from car in cars
+                           where car.Cost >= 50 && car.MaxSpeed >= 150
+                           select car;
+
+            foreach (var car in selected)
+            {
+                Console.WriteLine("Cost: {0}, MaxSpeed: {1}", car.Cost, car.MaxSpeed);
+            }
+        }
+    }
+}
