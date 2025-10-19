6d97890bd05dd88e8eaabf97eb97d212f9ac543e
Choi Yusun <43121372+ggstone0523@users.noreply.github.com>
2025-10-19 20:33:11 +0900

    Chap16: Add q1 answer

---
+++ Chap16/Chap16_q1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace Chap16_q1
+{
+    internal class Program
+    {
+        static void PrintMembers(Type type)
+        {
+            Console.WriteLine("-------- Members --------");
+
+            MemberInfo[] members = type.GetMembers();
+            foreach(MemberInfo i in members)
+                Console.WriteLine(i.Name);
+
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int a = 0;
+            Type type = a.GetType();
+
+            PrintMembers(type);
+        }
+    }
+}
