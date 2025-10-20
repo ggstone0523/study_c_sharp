da1ff0f3c054c7b33c0c2c0293432ccf27fb8226
Choi Yusun <43121372+ggstone0523@users.noreply.github.com>
2025-10-20 19:29:55 +0900

    Chap16: Add q2 answer

---
+++ Chap16/Chap16_q2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
+
+namespace Chap16_q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object myObject = new int();
+
+            Type t1 = myObject.GetType();
+            //Type t2 = typeof("int"); => error!
+            //Type t3 = Type.GetType(int); => error!
+            Type t4 = Type.GetType("System.Int32");
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t4);
+        }
+    }
+}
